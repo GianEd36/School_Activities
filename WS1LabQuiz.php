74bb22c479bed66cc3a00b7333a116426d278803
Gian Edmar Centeno <gcenteno_19ur0202@psu.edu.ph>
2024-09-19 23:07:16 +0800

    Update WS1LabQuiz.php
    Improved

--- WS1LabQuiz.php
+++ WS1LabQuiz.php
@@ -1,73 +1,91 @@
 <?php
-    echo 'Create an Array of Student Grades: ';
-    $grades = array(55, 60, 65, 70, 75, 80, 85, 90, 95, 100);
-    foreach ($grades as $grade) {
-        echo $grade.', ';
+    echo 'Create an Array of Student Grades: <br>';
+    $grades = [
+        "Gian"=>85, 
+        "Kyra"=>86,
+        "Ruan Mei"=>100, 
+        "Jediah"=>83,
+        "Yanna"=>75,  
+        "Jervan"=>84,  
+        "Pia"=>76, 
+        "Talyn"=>60,  
+        "Mike"=>79,
+        "Acheron"=>69,
+        "Hook"=>50
+    ];
+    foreach ($grades as $keys=>$value) {
+        echo $keys.': '.$value.'<br>';
     }
     //
     echo '<br>Create an Function to Calculate the Average Grade:';
     function calculateAverage(array $grades){
-        $average = 0;
-        foreach($grades as $grade){
-            $average += $grade;
-        }
-        return $average / 10;
+        return $average = array_sum($grades)/count($grades);//readable
     }
-    echo '<br>Average of the array is:'.calculateAverage(grades: $grades);
+    echo '<br>Average of the students is:'.calculateAverage($grades).'<br>';
     //
-    echo '<br>Create a function to Find the highest and lowest grades:';
+    echo '<br>Create a function to Find the highest and lowest grades:<br>';
     function findHighestGrade(array $grades){
-        $highest = 0;
-        foreach($grades as $grade){
-            if($highest < $grade){
-                $highest = $grade;
+        $highkey='';// We are declaring to catch the array values for the conditional value
+        $highval=null;// of the assoc array
+        foreach ($grades as $key => $value) {
+            if($highval === null || $value > $highval){
+                $highkey= $key;// catches the highest key
+                $highval= $value;// catches the value that we used for conditionals
             }
         }
-        return $highest;
+        return [$highkey=>$highval];
+        //return max($grades); // easiest way optional
     }
     function findLowestGrade(array $grades){
-        $lowest = $grades[0];
-        foreach($grades as $grade){
-            if($lowest > $grade){
-                $lowest = $grade;
+        $lkey = '';//same as highest but different naming convention
+        $lvalue = null;
+        foreach ($grades as $key => $value) {
+            if($lvalue === null || $value < $lvalue){//the difference
+                $lkey = $key;
+                $lvalue = $value;
             }
         }
-        return $lowest;
+        return [$lkey=>$lvalue];
+        //return min($grades);
     }
-    echo '<br>Highest grade:'.findHighestGrade(grades: $grades);
-    echo '<br>Lowest grade:'.findLowestGrade(grades: $grades).'<br>';
     //Create a function to Categorize Grades
     function categorizeGrades(array $grades){
-        $categorized = ['Excellent'=>[],'Good'=>[],'Average'=>[],'Pass'=>[],'Fail'=>[]];
-        foreach($grades as $grade){
-            if ($grade >= 90 && $grade <= 100) {
-                $categorized['Excellent'][] = $grade;  
+        $categorized = [];
+        //We're inserting the keys from the grades assoc arrays and giving it the rating
+        //on this category
+        foreach($grades as $keys=>$values){
+            if ($values >= 90 && $values <= 100) {
+                $categorized[$keys] = "Excellent";
             } 
-            elseif ($grade >= 80 && $grade < 90) {
-                $categorized['Good'][] = $grade;      
+            elseif ($values >= 80 && $values < 90) {
+                $categorized[$keys] = "Good";       
             } 
-            elseif ($grade >= 70 && $grade < 80) {             //UMAY NA UMAY NA AKO SA ASSOCIATIVE ARRAYS
-                $categorized['Average'][] = $grade;    
+            elseif ($values >= 70 && $values < 80) {             //UMAY NA UMAY NA AKO SA ASSOCIATIVE ARRAY
+                $categorized[$keys] = "Average";      
             } 
-            elseif ($grade >= 60 && $grade < 70) {
-                $categorized['Poor'][] = $grade;      
+            elseif ($values >= 60 && $values < 70) {
+                $categorized[$keys] = "Poor";       
             } 
-            elseif ($grade < 59) {
-                $categorized['Fail'][] = $grade;
+            else{
+                $categorized[$keys] = "Fail";   
             }
-            //echo "<br>".$categorized;
         }
         return $categorized;
     }
+
+    $highest=findHighestGrade($grades);
+    $lowest=findLowestGrade($grades);
     $results = categorizeGrades($grades);
-    // foreach($grades as $grade){
-    //     echo $grade.'<br>';
-    // }
-    echo 'Categorized grades:<br>';
-    foreach($results as $result=>$gradeArray){ 
-        foreach($gradeArray as $grade){
-            echo "$result = $grade <br>";
-        }
+
+    foreach($highest as $keys=> $values){
+        echo "The Highest grader of the class is $keys with a grade of : $values<br>";
+    }
+    foreach($lowest as $keys=> $values){
+        echo "The lowest grader of the class is $keys with a grade of : $values<br>";
+    }
+    echo '<br>Categorized grades:<br>';
+    foreach($results as $keys=>$values){ 
+        echo "$keys: $values<br>";
     }
 
 ?>
